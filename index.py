da96dce8734e7eacce29f073565d60433d889be3
Milton Figueredo <gratno@gmail.com>
2025-04-17 19:58:33 -0500

    Creando carpeta para HTML

--- index.py
+++ index.py
@@ -3,5 +3,13 @@ from flask import Flask
 #Aplicacion Flask = app
 app=Flask(__name__)
 
+@app.route('/') 
+def principal():
+      return   "🚀 Bienvenido a mi Web con Python 🤖"   
+
+@app.route('/contacto')
+def contacto():
+      return "Página de contacto"
+
 if __name__=='__main__':
-      app.run()  
+      app.run(debug=True, port=5017)  
