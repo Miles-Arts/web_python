5d2615f08e19c5a78b4c1d0380fc4c01338d201f
Milton <gratno@gmail.com>
2025-04-28 11:27:01 -0500

    Init proyect

--- example.py
+++ example.py
@@ -1,4 +1,2 @@
 print("Hola buenas tardes, ¿cómo estás? ")
-
-git config --global user.email gratno@gmail.com
-git config --global user.name Milton Figueredo
+print("Hola buenas tardes, ¿cómo estás? ")
